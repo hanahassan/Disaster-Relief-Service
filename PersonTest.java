0f41d2c516baa845a04c7d0ad398485f1e11394b
hanahassan <hana0hassan0@gmail.com>
2024-03-12 16:30:07 -0600

    person

--- PersonTest.java
+++ PersonTest.java
@@ -1,10 +1,31 @@
 package edu.ucalgary.oop;
 
-
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PersonTest {
 
+    private Person person;
+    private String expectedFirstName = "Hana";
+    private String expectedLastName = "Hassan";
 
+    @Before
+    public void setUp() {
+        person = new Person(expectedFirstName, expectedLastName);
+    }
+
+    @Test
+    public void testSetAndGetFirstName() {
+        String newFirstName = "Mena";
+        person.setFirstName(newFirstName);
+        assertEquals("setFirstName should update and getFirstName should return the new first name", newFirstName, person.getFirstName());
+    }
+
+    @Test
+    public void testSetAndGetLastName() {
+        String newLastName = "Bobby";
+        person.setLastName(newLastName);
+        assertEquals("setLastName should update and getLastName should return the new last name", newLastName, person.getLastName());
+    }
 }
